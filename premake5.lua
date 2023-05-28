36eefcaabf5bbd7eba160fb339027eb2be0c8a5a
aptmoo <socrates.de.filosoof.ll@gmail.com>
2023-05-28 15:18:40 +0200

    Added premake file, but for real now

---
+++ premake5.lua
@@ -0,0 +1,35 @@
+workspace "2dgame"
+    configurations { "Debug", "Release" }
+
+project "game"
+    -- Input
+    files
+    {
+        "src/**.h", "src/**.c"
+    }
+
+    includedirs
+    {
+        "src/"
+    }
+
+    -- Linker
+    links
+    {
+        "GL"
+    }
+
+    filter "system:linux"
+        links { "dl", "m" }
+        libdirs { "/usr/lib/" }
+    
+
+    -- Language
+    language    "C"
+    cdialect    "C17"
+
+    -- Output
+    kind        "ConsoleApp"
+    targetname  "2dgame.out"
+    targetdir   "./bin/"
+    objdir      "./bin-int/"
